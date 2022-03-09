a89d53fd7f06f92d19fe56c7050eda4c606014a6
Luce <1594668000@qq.com>
2022-03-09 19:24:14 +0800

    1. 新增gateway子模块，增加gateway依赖，并注册到注册中心

---
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -0,0 +1,33 @@
+package com.course.gateway;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+
+	private static final Logger LOG = (Logger) LoggerFactory.getLogger(GatewayApplication.class);
+
+	public static void main(String[] args)
+	{
+		SpringApplication app = new SpringApplication(GatewayApplication.class);
+		Environment env = (Environment) app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
+
+
+	}
+
+
+
+
+}
