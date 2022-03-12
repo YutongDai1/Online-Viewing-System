925dee37585727bc8eef21c5c803b41af8a5b72a
Luce <1594668000@qq.com>
2022-03-12 16:33:17 +0800

    1. 点击sidebar菜单实现页面跳转并增加激活样式

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -981,3 +981,17 @@
     </tbody>
   </table>
 </template>
+
+
+<script>
+
+  export default {
+    name: 'chapter',
+    mounted: function () {
+      this.$parent.activeSidebar("business-chapter-sidebar");
+    },
+    methods: {}
+
+  }
+</script>
+
