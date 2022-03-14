dec1cebee528f6c4ba1cd6f3544eafce23133865
Luce <1594668000@qq.com>
2022-03-14 21:54:02 +0800

    1. 制作toast组件，内部用sweetalert2实现

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -161,11 +161,7 @@
               let resp = response.data;
               if (resp.success) {
                 _this.list(1);
-                Swal.fire(
-                  '删除成功!',
-                  '删除成功！',
-                  'success'
-                )
+                toast.success("删除成功");
               }
             })
           }
@@ -196,6 +192,7 @@
           if (resp.success) {
             $("#form-modal").modal("hide");
             _this.list(1);
+            toast.success("保存成功");
           }
         })
       }
