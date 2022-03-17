f3949b58c2b19f753c5affb3f8100ab45dc48e0c
Luce <1594668000@qq.com>
2022-03-18 00:01:57 +0800

    1. 增加枚举生成器EnumGenerator 2. optionKV过滤器从list改为object

--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -36,7 +36,7 @@
         <td>{{section.chapterId}}</td>
         <td>{{section.video}}</td>
         <td>{{section.time}}</td>
-        <td>{{CHARGE | optionKV(section.charge)}}</td>
+        <td>{{SECTION_CHARGE | optionKV(section.charge)}}</td>
         <td>{{section.sort}}</td>
         <td>
           <div class="hidden-sm hidden-xs btn-group">
@@ -132,7 +132,7 @@
                 <label class="col-sm-2 control-label">收费</label>
                 <div class="col-sm-10">
                   <select v-model="section.charge" class="form-control">
-                    <option v-for="o in CHARGE" v-bind:value="o.key">{{o.value}}</option>
+                    <option v-for="o in SECTION_CHARGE" v-bind:value="o.key">{{o.value}}</option>
                   </select>
                 </div>
               </div>
@@ -165,7 +165,7 @@
       return {
         section: {},
         sections: [],
-        CHARGE: CHARGE,
+        SECTION_CHARGE: SECTION_CHARGE,
       }
     },
     mounted: function () {
