eedde040bfa3fed06835a98b3eaf4d995363c375
Luce <1594668000@qq.com>
2022-03-20 20:16:44 +0800

    1. 保存课程时，同时保存课程分类 2. 增加课程分类关联表，一门课程可以属于多个分类

---
+++ server/src/main/java/com/dyt/server/service/CourseCategoryService.java
@@ -0,0 +1,97 @@
+package com.dyt.server.service;
+
+import com.dyt.server.domain.CourseCategory;
+import com.dyt.server.domain.CourseCategoryExample;
+import com.dyt.server.dto.CategoryDto;
+import com.dyt.server.dto.CourseCategoryDto;
+import com.dyt.server.dto.PageDto;
+import com.dyt.server.mapper.CourseCategoryMapper;
+import com.dyt.server.util.CopyUtil;
+import com.dyt.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@Service
+public class CourseCategoryService {
+
+    @Resource
+    private CourseCategoryMapper courseCategoryMapper;
+
+    /**
+     * 列表查询
+     */
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
+        CourseCategoryExample courseCategoryExample = new CourseCategoryExample();
+
+        List<CourseCategory> courseCategorys = courseCategoryMapper.selectByExample(courseCategoryExample);
+
+        PageInfo<CourseCategory> pageInfo = new PageInfo<>(courseCategorys);
+        pageDto.setTotal(pageInfo.getTotal());
+        List
+                <CourseCategoryDto> courseCategoryDtos = CopyUtil.copyList(courseCategorys, CourseCategoryDto.class);
+
+        pageDto.setList(courseCategoryDtos);
+
+    }
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    public void save(CourseCategoryDto courseCategoryDto) {
+        CourseCategory courseCategory = new CourseCategory();
+        BeanUtils.copyProperties(courseCategoryDto, courseCategory);
+        if (StringUtils.isEmpty(courseCategoryDto.getId())) {
+            this.insert(courseCategory);
+        } else {
+            this.update(courseCategory);
+        }
+    }
+
+    /**
+     * 新增
+     */
+    public void insert(CourseCategory courseCategory) {
+
+
+        courseCategory.setId(UuidUtil.getShortUuid());
+        courseCategoryMapper.insert(courseCategory);
+    }
+
+    /**
+     * 更新
+     */
+    public void update(CourseCategory courseCategory) {
+
+        courseCategoryMapper.updateByPrimaryKey(courseCategory);
+    }
+
+    /**
+     * 删除
+     */
+    public void delete(String id) {
+        courseCategoryMapper.deleteByPrimaryKey(id);
+    }
+
+    public void saveBatch(String courseId, List<CategoryDto> dtoList) {
+        CourseCategoryExample example = new CourseCategoryExample();
+        example.createCriteria().andCourseIdEqualTo(courseId);
+        courseCategoryMapper.deleteByExample(example);
+        for (int i = 0, l = dtoList.size(); i < l; i++) {
+            CategoryDto categoryDto = dtoList.get(i);
+            CourseCategory courseCategory = new CourseCategory();
+            courseCategory.setId(UuidUtil.getShortUuid());
+            courseCategory.setCourseId(courseId);
+            courseCategory.setCategoryId(categoryDto.getId());
+            insert(courseCategory);
+        }
+    }
+
+}
--- server/src/main/java/com/dyt/server/service/CourseService.java
+++ server/src/main/java/com/dyt/server/service/CourseService.java
@@ -29,6 +29,9 @@ public class CourseService {
     @Resource
     private MyCourseMapper myCourseMapper;
 
+    @Resource
+    private CourseCategoryService courseCategoryService;
+
     /**
      * 列表查询
      */
@@ -59,6 +62,9 @@ public class CourseService {
         } else {
             this.update(course);
         }
+
+        // 批量保存课程分类
+        courseCategoryService.saveBatch(courseDto.getId(), courseDto.getCategorys());
     }
 
     /**
