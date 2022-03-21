3076ad965e2b49f2740c1905f7413ae7d783968d
Luce <1594668000@qq.com>
2022-03-21 19:21:12 +0800

    1. 增加讲师管理功能

---
+++ server/src/main/java/com/dyt/server/service/TeacherService.java
@@ -0,0 +1,83 @@
+package com.dyt.server.service;
+
+import com.dyt.server.domain.Teacher;
+import com.dyt.server.domain.TeacherExample;
+import com.dyt.server.dto.PageDto;
+import com.dyt.server.dto.TeacherDto;
+import com.dyt.server.mapper.TeacherMapper;
+import com.dyt.server.util.CopyUtil;
+import com.dyt.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@Service
+public class TeacherService {
+
+    @Resource
+    private TeacherMapper teacherMapper;
+
+    /**
+     * 列表查询
+     */
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
+        TeacherExample teacherExample = new TeacherExample();
+
+        List<Teacher> teachers = teacherMapper.selectByExample(teacherExample);
+
+        PageInfo<Teacher> pageInfo = new PageInfo<>(teachers);
+        pageDto.setTotal(pageInfo.getTotal());
+        List
+                <TeacherDto> teacherDtos = CopyUtil.copyList(teachers, TeacherDto.class);
+
+        pageDto.setList(teacherDtos);
+
+    }
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    public void save(TeacherDto teacherDto) {
+        Teacher teacher = new Teacher();
+        BeanUtils.copyProperties(teacherDto, teacher);
+        if (StringUtils.isEmpty(teacherDto.getId())) {
+            this.insert(teacher);
+        } else {
+            this.update(teacher);
+        }
+    }
+
+    /**
+     * 新增
+     */
+    public void insert(Teacher teacher) {
+
+
+        teacher.setId(UuidUtil.getShortUuid());
+        teacherMapper.insert(teacher);
+    }
+
+    /**
+     * 更新
+     */
+    public void update(Teacher teacher) {
+
+        teacherMapper.updateByPrimaryKey(teacher);
+    }
+
+    /**
+     * 删除
+     */
+    public void delete(String id) {
+        teacherMapper.deleteByPrimaryKey(id);
+    }
+
+
+}
