4469c045eabd059ffdb17cf57b2bb3cacdc57d4c
Luce <1594668000@qq.com>
2022-03-22 15:03:21 +0800

    1. 文件上传功能开发：文件上传实时显示与保存

--- file/src/main/java/com/dyt/file/config/SpringMvcConfig.java
+++ file/src/main/java/com/dyt/file/config/SpringMvcConfig.java
@@ -9,6 +9,6 @@ public class SpringMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/f/**").addResourceLocations("file:D:/spring cloud+vue/file/course");
+        registry.addResourceHandler("/f/**").addResourceLocations("file:D:/spring cloud+vue/file/course/");
     }
 }
