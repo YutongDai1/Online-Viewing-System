2d1ad1d1da3ad386f2e86e0f3824215b62011c79
Luce <1594668000@qq.com>
2022-03-25 19:06:35 +0800

    1. 大文件断点续传与极速秒传：使用文件生成md5签名，作为文件标识

--- file/src/main/java/com/dyt/file/controller/admin/UploadController.java
+++ file/src/main/java/com/dyt/file/controller/admin/UploadController.java
@@ -5,7 +5,6 @@ import com.dyt.server.dto.FileDto;
 import com.dyt.server.dto.ResponseDto;
 import com.dyt.server.enums.FileUseEnum;
 import com.dyt.server.service.FileService;
-import com.dyt.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -46,12 +45,12 @@ public class UploadController {
                               Integer size,
                               Integer shardIndex,
                               Integer shardSize,
-                              Integer shardTotal) throws IOException {
+                              Integer shardTotal,
+                              String key) throws IOException {
         LOG.info("上传文件开始");
 
         // 保存文件到本地
         FileUseEnum useEnum = FileUseEnum.getByCode(use);
-        String key = UuidUtil.getShortUuid();
 
         //如果文件夹不存在则创建
         String dir = useEnum.name().toLowerCase();
