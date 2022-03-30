3abe84a16f566a460a0015ed1aa9b3dc6128af3a
Luce <1594668000@qq.com>
2022-03-30 21:54:16 +0800

    1. 视频加密与授权播放：基于OSS原生SDK上传视频到点播服务，上传到指定分类并自动转码

--- server/src/main/java/com/dyt/server/util/VodUtil.java
+++ server/src/main/java/com/dyt/server/util/VodUtil.java
@@ -45,9 +45,9 @@ public class VodUtil {
         request.setTitle("this is title");
         //request.setDescription("this is desc");
         //request.setTags("tag1,tag2");
-        //request.setCoverURL("http://vod.aliyun.com/test_cover_url.jpg");
-        //request.setCateId(-1L);
-        //request.setTemplateGroupId("");
+        request.setCoverURL("http://vod.aliyun.com/test_cover_url.jpg");
+        request.setCateId(1000388932L);
+        request.setTemplateGroupId("b3e112ac41e2edf4d0e78e187f6de96c");
         //request.setWorkflowId("");
         //request.setStorageLocation("");
         //request.setAppId("app-1000000");
