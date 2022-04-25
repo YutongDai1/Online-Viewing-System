4410ada7f6260216ae442ee5e9fa1a022b03987c
Luce <1594668000@qq.com>
2022-04-25 21:40:43 +0800

    1. 网站首页开发，底部文案修改

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -2,11 +2,10 @@
   <footer class="text-muted">
     <div class="container">
       <p class="float-right">
-        <a href="#">Back to top</a>
+        <a href="#">回到顶部</a>
       </p>
-      <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-      <p>New to Bootstrap? <a href="https://v4.bootcss.com/">Visit the homepage</a> or read our <a
-        href="/docs/getting-started/introduction/">getting started guide</a>.</p>
+      <p>欢迎使用在线视频系统，尽情享受视频的乐趣</p>
+      <p>2099&nbsp;video.com&nbsp;&nbsp;京ICP备 1234567890号</p>
     </div>
   </footer>
 </template>
