6450e569cfb0d38325ef7ae4843f20cd1380c9b5
Luce <1594668000@qq.com>
2022-05-02 16:33:23 +0800

    1. 会员报名课程功能开发，加载完课程信息后，如果已登录，则加载报名信息。

--- business/src/main/java/com/dyt/business/controller/web/MemberCourseController.java
+++ business/src/main/java/com/dyt/business/controller/web/MemberCourseController.java
@@ -35,5 +35,15 @@ public class MemberCourseController {
         return responseDto;
     }
 
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    @PostMapping("/get-enroll")
+    public ResponseDto getEnroll(@RequestBody MemberCourseDto memberCourseDto) {
+        ResponseDto responseDto = new ResponseDto();
+        memberCourseDto = memberCourseService.getEnroll(memberCourseDto);
+        responseDto.setContent(memberCourseDto);
+        return responseDto;
+    }
 
 }
